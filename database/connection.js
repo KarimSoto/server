aec913dff748615b4cb02e1ed8932129be044693
KarimSoto <karimtec2003@gmail.com>
2024-12-04 21:49:57 -0800

    Cambios en el connection.js

--- database/connection.js
+++ database/connection.js
@@ -4,11 +4,11 @@ dotenv.config({path:'../.env'});
 
 
 const connection = mysql.createConnection({
-    host:"autorack.proxy.rlwy.net",
-    port:16467,
-    database:"railway ",
-    user:"root",
-    password:"NMJztHxpfQNbcMtSJQgpKsWRWvVqsrkM"
+    host:process.env.MySQL_host || "autorack.proxy.rlwy.net",
+    port: process.env.MySQL_port ||16467,
+    database: process.env.MySQL_database ||  "railway ",
+    user: process.env.MySQL_user || "root",
+    password: process.env.MySQL_password || "NMJztHxpfQNbcMtSJQgpKsWRWvVqsrkM"
 }).promise();
 
 
