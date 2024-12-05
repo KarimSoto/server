8f0860f7729ae8332ed8188862bee53b188fe5d9
KarimSoto <karimtec2003@gmail.com>
2024-12-04 21:57:26 -0800

    Cambios espacio

--- database/connection.js
+++ database/connection.js
@@ -6,7 +6,7 @@ dotenv.config({path:'../.env'});
 const connection = mysql.createConnection({
     host:process.env.MySQL_host || "autorack.proxy.rlwy.net",
     port: process.env.MySQL_port ||16467,
-    database: process.env.MySQL_database ||  "railway ",
+    database: process.env.MySQL_database ||  "railway",
     user: process.env.MySQL_user || "root",
     password: process.env.MySQL_password || "NMJztHxpfQNbcMtSJQgpKsWRWvVqsrkM"
 }).promise();
